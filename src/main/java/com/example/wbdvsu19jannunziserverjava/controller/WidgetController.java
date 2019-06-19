cafcf64b457c34fc2150b93a708c117709c3088f
mansikul26@gmail.com <kulkarni.man@husky.neu.edu>
2019-06-18 22:44:27 -0400

    Allowed credentials

--- src/main/java/com/example/wbdvsu19jannunziserverjava/controller/WidgetController.java
+++ src/main/java/com/example/wbdvsu19jannunziserverjava/controller/WidgetController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*" , allowCredentials = "true" , allowedHeaders = "*")
 public class WidgetController {
 
     @Autowired
