1111b5e305094dc165e8a4ab3e0a11c661517374
mansikul26@gmail.com <kulkarni.man@husky.neu.edu>
2019-06-21 23:26:32 -0400

    changes in controller

--- src/main/java/com/example/wbdvsu19jannunziserverjava/controller/ModuleController.java
+++ src/main/java/com/example/wbdvsu19jannunziserverjava/controller/ModuleController.java
@@ -16,49 +16,39 @@ import java.util.List;
 @CrossOrigin(origins = "*" , allowCredentials = "true" , allowedHeaders = "*")
 public class ModuleController {
 
-    @Autowired
-    ModuleRepository repository;
-
-    @Autowired
-    CourseRepository courseRepository;
+	 @Autowired
+	 ModuleService service;
 
     @GetMapping("/api/modules")
     public List<Module> findAllModules() {
-        return repository.findAllModules();
+        return service.findAllModules();
     }
 
     @GetMapping("/api/courses/{cid}/modules")
     public List<Module> findAllModulesForCourse(
             @PathVariable("cid") Integer courseId) {
-        return repository.findAllModulesForCourse(courseId);
+        return service.findAllModulesForCourse(courseId);
     }
 
     @PostMapping("/api/courses/{cid}/modules")
     public List<Module> addModuleToCourse(
             @PathVariable("cid") Integer courseId,
             @RequestBody Module newModule) {
-        Course course = courseRepository.findCourseById(courseId);
-        newModule.setCourse(course);
-        repository.save(newModule);
-        return repository.findAllModulesForCourse(courseId);
+        return service.addModuleToCourse(courseId, newModule);
     }
 
     @GetMapping("/api/modules/{mid}")
     public Module findModuleById(@PathVariable("mid") Integer id) {
-        return repository.findModuleById(id);
+        return service.findModuleById(id);
     }
     
     @PutMapping("/api/modules/{mid}")
 	public Module updateModule(@PathVariable("mid") int mid, @RequestBody Module module) {
-		Module m =  findModuleById(mid);
-		m.setId(module.getId());
-		m.setTitle(module.getTitle());
-		return repository.save(m);
+		return service.updateModule(mid, module);
 	}
 	
 	@DeleteMapping("/api/modules/{mid}")
 	public void deleteModule(@PathVariable("mid") int mid) {
-		repository.deleteById(mid);
+		service.deleteModule(mid);
 	}
 }
-
